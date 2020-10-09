207cc5f8e36db1db3a879a7734d21e11d6ab071c
Xiaoxian Yang <00100282@uwa.edu.au>
2020-10-09 16:05:37 +0800

    update

--- SUB/prebenzenefittingall.m
+++ SUB/prebenzenefittingall.m
@@ -16,15 +16,15 @@ ndata = size(xdata,1);
 ydata = zeros(ndata,1);
 for idata = 1:ndata
     if xdata(idata,4) == 1
-        ydata(idata)=(benzenePropsfitV(a1,xdata(idata,1:2))-xdata(idata,3))^2;
+        ydata(idata)=(benzenePropsfitV(a1,xdata(idata,1:2))*xdata(idata,4)-xdata(idata,3))^2;
     elseif xdata(idata,4) == 4
-        ydata(idata)=(benzenePropsfitalpha(a1,xdata(idata,1:2))-xdata(idata,3))^2;
+        ydata(idata)=(benzenePropsfitalpha(a1,xdata(idata,1:2))*xdata(idata,4)-xdata(idata,3))^2;
     elseif xdata(idata,4) == 5
-        ydata(idata)=(benzenePropsfitcp(a1,xdata(idata,1:2))-xdata(idata,3))^2;
+        ydata(idata)=(benzenePropsfitcp(a1,xdata(idata,1:2))*xdata(idata,4)-xdata(idata,3))^2;
     elseif xdata(idata,4) == 6
-        ydata(idata)=(benzenePropsfitcv(a1,xdata(idata,1:2))-xdata(idata,3))^2;
+        ydata(idata)=(benzenePropsfitcv(a1,xdata(idata,1:2))*xdata(idata,4)-xdata(idata,3))^2;
     elseif xdata(idata,4) == 11
-        ydata(idata)=(benzenePropsfitH(a1,xdata(idata,1:2))-xdata(idata,3))^2;
+        ydata(idata)=(benzenePropsfitH(a1,xdata(idata,1:2))*xdata(idata,4)-xdata(idata,3))^2;
     else
         error('Flag can only be 1 4 5 6 11')
     end   
